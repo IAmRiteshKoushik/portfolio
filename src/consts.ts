dfbcda6504752b625e273ed3d46586cd337b5bfc
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-08-17 16:04:55 +0530

    Improve contents in landing page

--- src/consts.ts
+++ src/consts.ts
@@ -3,8 +3,8 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "Ritesh Koushik",
   EMAIL: "riteshkoushik39@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 1,
-  NUM_WORKS_ON_HOMEPAGE: 1,
+  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 2,
 };
 
