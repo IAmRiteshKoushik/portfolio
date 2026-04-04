e1f22fb6a71edf7c217619e8f43799054375dd63
Ritesh Koushik <riteshkoushik39@gmail.com>
2026-04-04 15:45:44 +0530

    fix formatting of config page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,35 +1,45 @@
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional()
-  }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		date: z.coerce.date(),
+		draft: z.boolean().optional(),
+	}),
 });
 
 const work = defineCollection({
-  type: "content",
-  schema: z.object({
-    company: z.string(),
-    role: z.string(),
-    dateStart: z.coerce.date(),
-    dateEnd: z.union([z.coerce.date(), z.string()]),
-  }),
+	type: "content",
+	schema: z.object({
+		company: z.string(),
+		role: z.string(),
+		dateStart: z.coerce.date(),
+		dateEnd: z.union([z.coerce.date(), z.string()]),
+	}),
 });
 
 const projects = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
-    demoURL: z.string().optional(),
-    repoURL: z.string().optional()
-  }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		date: z.coerce.date(),
+		draft: z.boolean().optional(),
+		demoURL: z.string().optional(),
+		repoURL: z.string().optional(),
+	}),
 });
+
+// const til = defineCollection({
+// 	type: "til",
+// 	schema: z.object({
+// 		title: z.string(),
+// 		description: z.string(),
+// 		date: z.coerce.date(),
+// 		draft: z.boolean().optional(),
+// 	}),
+// });
 
 export const collections = { blog, work, projects };
