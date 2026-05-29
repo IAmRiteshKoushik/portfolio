d97ae8c972201a502d46fafa641ca091fc0e116a
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2026-05-30 01:15:24 +0530

    increment works on homepage

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   NAME: "Ritesh Koushik",
   EMAIL: "riteshkoushik39@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
-  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_WORKS_ON_HOMEPAGE: 3,
   NUM_PROJECTS_ON_HOMEPAGE: 2,
 };
 
